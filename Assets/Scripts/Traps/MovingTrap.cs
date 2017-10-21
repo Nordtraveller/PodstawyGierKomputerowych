04f3eb0b073c89ff4ed0d2f024685ec95933697f
203946 Mirkiewicz Kinga <203946@edu.p.lodz.pl>
2017-10-21 11:17:24 +0200

    Moving Trap Fix

--- Assets/Scripts/Traps/MovingTrap.cs
+++ Assets/Scripts/Traps/MovingTrap.cs
@@ -7,25 +7,22 @@ public class MovingTrap : MonoBehaviour
 {
     private float startingX;
     private float randomStartTime;
+    private float x;
     private float speed = 1f;
     private float time;
 
     private void Start()
     {
-        startingX = transform.position.x + 1;
-        randomStartTime = Random.Range(0.0f, 0.5f);
+        startingX = transform.position.x;
     }
 
     void Update()
     {
-        time += Time.deltaTime;
-        if (time >= randomStartTime)
-        {
             Vector3 pointA = new Vector3(startingX, this.transform.position.y, this.transform.position.z);
-            Vector3 pointB = pointA + new Vector3((GetComponentInParent<Trap>().size - 1), 0, 0);
+            Vector3 pointB = pointA + new Vector3((GetComponentInParent<Trap>().size - GameMetrics.tileHorizontalSize), 0, 0);
             float pingPong = Mathf.PingPong(Time.time * speed, 1);
             this.transform.position = Vector3.Lerp(pointA, pointB, pingPong);
-        }
+
             
     }
 }

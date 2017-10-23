2a9232c3a319d774dcfd10c2c46fe8a87cdea89d
Piotr Morawski <piter.morawski@gmail.com>
2017-10-23 22:37:58 +0200

    Dodanie turretTrap

---
+++ Assets/Scripts/Traps/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector3 bulletStartingPosition;
+
+    public float bulletSpeed = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+        bulletStartingPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - bulletSpeed * Time.deltaTime, transform.position.y,
+            transform.position.z);
+
+        if (bulletStartingPosition.x - transform.position.x > 6)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Traps/TurretTrap.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretTrap : MonoBehaviour
+{
+    public bool currentFloor = false; //strzela tylko jeśli jest na aktywnym florze
+
+    public bool shootingLeft = true; // strzela w lewo
+
+    public bool reloadingAmmo = false; //czy przeładowuje
+
+    public float reloadingTimmer;
+
+    public float reloadInterval;
+
+    public GameObject bullet; //pocisk
+
+
+    // Use this for initialization
+    void Start()
+    {
+        reloadInterval = 4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FloorCheck();
+        if (currentFloor && !reloadingAmmo)
+        {
+            shoot();
+        }
+        reloadingCheck();
+    }
+
+    void FloorCheck()
+    {
+        if (transform.position.y <= 1.4f)
+        {
+            currentFloor = true;
+        }
+        else
+        {
+            currentFloor = false;
+        }
+    }
+
+    void reloadingCheck()
+    {
+        reloadingTimmer += Time.deltaTime;
+        if (reloadingTimmer >= reloadInterval)
+        {
+            reloadingAmmo = false;
+        }
+        else
+        {
+            reloadingAmmo = true;
+        }
+    }
+
+
+    void shoot()
+    {
+        Vector3 bulletStartingPosition =
+            new Vector3(transform.position.x - 0.7f, transform.position.y, transform.position.z);
+        GameObject bulletInstance;
+        bulletInstance = Instantiate(bullet, bulletStartingPosition, transform.rotation) as GameObject;
+        reloadingAmmo = true;
+        reloadingTimmer = 0;
+    }
+}

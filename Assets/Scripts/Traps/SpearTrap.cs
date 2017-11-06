4bb66f74e7f712de480d34587811af8537a677c3
Piotr Morawski <piter.morawski@gmail.com>
2017-11-06 21:28:12 +0100

    Dodanie Menu umozliwiajacego pauzowanie gry, restart, powrót do gry, wyjście z gry, przejscie do opcji zaawansowanych. Dodanie nowego trapa - wysuwająca się włócznia

---
+++ Assets/Scripts/Traps/SpearTrap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearTrap : MonoBehaviour {
+
+    private float startingY;
+    private float attackToY;
+    private float attackSpeed = 1f;
+
+    private float attackRechargeTimmer;
+    private float attackRechargeInterval = 1.5f;
+
+    private bool kek = false;
+
+    // Use this for initialization
+    void Start () {
+        startingY = transform.position.y;
+        attackToY = transform.position.y + 0.9f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        /*   if (attackRechargeTimmer < attackRechargeInterval)
+           {
+               attackRechargeTimmer += Time.deltaTime;
+           }
+           else
+           {
+               attack();
+               if (transform.position.y < startingY + 0.05f)
+               {
+                   this.transform.position.Set(this.transform.position.x, this.transform.position.y + 0.06f, this.transform.position.z);
+                   attackRechargeTimmer = 0;
+
+               }
+           }*/
+           attack();
+
+    }
+
+
+    void attack()
+    {
+        Vector3 start = new Vector3(this.transform.position.x, startingY, this.transform.position.z);
+        Vector3 end = new Vector3(this.transform.position.x, attackToY, this.transform.position.z);
+        float interpolant = Mathf.PingPong(Time.time * attackSpeed, 1f);
+        this.transform.position = Vector3.Lerp(start, end, interpolant);
+    }
+}
---
+++ Assets/Scripts/UIITems/PauseMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+
+
+    public GameObject PauseObject;
+
+    private bool IsPaused = false;
+
+    // Use this for initialization
+    void Start () {
+		PauseObject.SetActive(false);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetButtonDown("Pause"))
+	    {
+	        IsPaused = !IsPaused;
+
+	    }
+
+	    if (IsPaused)
+	    {
+	        PauseObject.SetActive(true);
+	        Time.timeScale = 0;
+	    }
+
+	    if (!IsPaused)
+	    {
+	        PauseObject.SetActive(false);
+	        Time.timeScale = 1;
+        }
+	}
+
+    public void NewGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(0);
+    }
+
+    public void Resume()
+    {
+        IsPaused = false;
+    }
+
+    public void Options()
+    {
+        //SceneManager.LoadScene(1); -> difficulty, graphic, ?skins?
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}

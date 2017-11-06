8d0cf8db902ecd7aa99f17daa1467eb81b9d66db
203890 Jaros Olgierd <203890@edu.p.lodz.pl>
2017-11-06 10:50:09 +0100

    Usuwanie particli
    Particle usuwają się gdy piętro przestaje być aktywne.

---
+++ Assets/Scripts/ParticleDelete.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDelete : MonoBehaviour {
+
+    Transform floor;
+	// Use this for initialization
+	void Start ()
+    {
+        floor = transform.parent.parent;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(floor.position.y <= -1)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}

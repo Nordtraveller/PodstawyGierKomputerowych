f6aef560a997de0a21434d4feff7bb59e673569d
Piotr Morawski <piter.morawski@gmail.com>
2017-11-19 20:30:22 +0100

    Logika działania KeyHUD oraz klucz w paincie

---
+++ Assets/Scripts/UIITems/KeyHUD.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyHUD : MonoBehaviour
+{
+
+    public Sprite[] Keys;
+
+    public Image KeyUI;
+
+    private PlayerStatus PlayerStatus;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    PlayerStatus = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatus>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (PlayerStatus.haveKey)
+	    {
+	        KeyUI.sprite = Keys[1];
+	    }
+	    else
+	    {
+	        KeyUI.sprite = Keys[0];
+        }
+	}
+}

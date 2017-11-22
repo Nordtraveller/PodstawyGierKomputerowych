3300cc4c830bd8bbe97bd6b22e2a28e370d35acc
Piotr Morawski <piter.morawski@gmail.com>
2017-11-23 00:43:24 +0100

    zabawa z keyIndicatorem, not done properly yet

--- Assets/Scripts/UIITems/KeyIndicator.cs
+++ Assets/Scripts/UIITems/KeyIndicator.cs
@@ -8,20 +8,48 @@ public class KeyIndicator : MonoBehaviour {
 
     public GameObject Full;
 
+    private PlayerStatus PlayerStatus;
+
+    private GameObject keyEmptyInstance;
+
+    private GameObject keyFullInstance;
+
+    private float emptyCreateTimmer;
+
+    private float emptyCreateInterval = 3.0f;
+
     // Use this for initialization
     void Start () {
-        GameObject keyInstance;
-        keyInstance = Instantiate(Empty, transform.position, transform.rotation) as GameObject;
+        PlayerStatus = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatus>();
+
+        keyEmptyInstance = Instantiate(Empty, transform.position, transform.rotation) as GameObject;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+	    emptyCreateTimmer += Time.deltaTime;
+
+        if (PlayerStatus.haveKey && keyFullInstance == null)
+	    {
+            Destroy(keyEmptyInstance);
+	        keyFullInstance = Instantiate(Full, transform.position, transform.rotation) as GameObject;
+	        emptyCreateTimmer = 0.0f;
+	    }
+	    else if(!PlayerStatus.haveKey && keyFullInstance != null)
+	    {
+            Destroy(keyFullInstance);
+	        emptyCreateTimmer = 0.0f;
+            
+        }
+        else if (emptyCreateTimmer >= emptyCreateInterval && keyEmptyInstance == null)
+        {
+            keyEmptyInstance = Instantiate(Empty, transform.position, transform.rotation) as GameObject;
+        }
+    }
 
 
-    private void OnTriggerEnter(Collider collider)
+/*    private void OnTriggerEnter(Collider collider)
     {
         Destroy(gameObject);
-    }
+    }*/
 }

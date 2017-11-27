9d04e9c51d88c2505138834b2806ccb55b868c20
Piotr Morawski <piter.morawski@gmail.com>
2017-11-27 17:08:31 +0100

    HUD a bit

--- Assets/Scripts/UIITems/KeyHUD.cs
+++ Assets/Scripts/UIITems/KeyHUD.cs
@@ -6,28 +6,68 @@ using UnityEngine.UI;
 public class KeyHUD : MonoBehaviour
 {
 
-    public Sprite[] Keys;
+    public Sprite[] Sprites;
 
     public Image KeyUI;
 
+    public Image TeleportUI;
+
+    public Image GoldenKeyUI;
+
+    public Image TrapDestroyerUI;
+
     private PlayerStatus PlayerStatus;
 
-	// Use this for initialization
-	void Start ()
+    private float AlphaOfCollected = 1.0f;
+
+    private float AlphaOfNotCollected = 0.1f;
+
+    // Use this for initialization
+    void Start ()
 	{
 	    PlayerStatus = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatus>();
-	}
+
+        TeleportUI.sprite = Sprites[2];
+        GoldenKeyUI.sprite = Sprites[3];
+        TrapDestroyerUI.sprite = Sprites[4];
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
 	    if (PlayerStatus.haveKey)
 	    {
-	        KeyUI.sprite = Keys[1];
+	        KeyUI.sprite = Sprites[1];
 	    }
 	    else
 	    {
-	        KeyUI.sprite = Keys[0];
+	        KeyUI.sprite = Sprites[0];
         }
-	}
+
+	    if (PlayerStatus.hasExtraKey)
+	    {
+            alphaSwapper(GoldenKeyUI, AlphaOfCollected);
+	    }
+	    else
+	    {
+	        alphaSwapper(GoldenKeyUI, AlphaOfNotCollected);
+        }
+
+	    if (PlayerStatus.hasExtraTeleport)
+	    {
+	        alphaSwapper(TeleportUI, AlphaOfCollected);
+        }
+	    else
+	    {
+	        alphaSwapper(TeleportUI, AlphaOfNotCollected);
+        }
+    }
+
+
+    private void alphaSwapper(Image image, float alphaValue)
+    {
+        Color tmp = image.GetComponent<SpriteRenderer>().color;
+        tmp.a = alphaValue;
+        image.GetComponent<SpriteRenderer>().color = tmp;
+    }
 }

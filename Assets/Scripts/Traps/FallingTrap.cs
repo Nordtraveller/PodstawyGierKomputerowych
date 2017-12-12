96cba96d478f0ca704d6b377e7a40ce70a13b6dd
olhavas <olickav@gmail.com>
2017-12-12 03:01:47 +0100

    Podstawowa wersja dzialajaca z spadajacym trapem i na tle nie ma cieniow

---
+++ Assets/Scripts/Traps/FallingTrap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTrap : MonoBehaviour {
+
+    private bool isActive;
+    private float startYPosition;
+    private float endYPosition;
+    private Vector3 start;
+    private Vector3 target;
+    private float speed = 14f;
+
+
+    // Use this for initialization
+    void Start () {
+        isActive = false;
+
+        startYPosition =(float)GameObject.FindWithTag("LevelCreator").GetComponent<LevelCreator>()
+            .getUpperFloor().transform.position.y;
+        endYPosition = (float)GameObject.FindWithTag("LevelCreator").GetComponent<LevelCreator>()
+                                                   .actualFloor.transform.position.y;
+        transform.position = new Vector3(transform.position.x, startYPosition -1.5f, transform.position.z);
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float playerX = GameObject.FindWithTag("Player").GetComponent<PlayerControlls>().transform.position.x;
+        if (((this.transform.position.x + 0.9f) >= playerX) &&
+            ((this.transform.position.x - 0.9f) <= playerX))
+        {
+            this.isActive = true;
+        }
+
+        if (isActive)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, transform.position.y - step,
+            transform.position.z);
+            Debug.Log(transform.position.y);
+        }
+
+        if (transform.position.y == endYPosition+2f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

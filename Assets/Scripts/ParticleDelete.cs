e0be14c83cbbfc48f844ed14d5bdfcfacff348b9
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-02-02 19:00:00 +0100

    Little project cleaning

--- Assets/Scripts/ParticleDelete.cs
+++ Assets/Scripts/ParticleDelete.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class ParticleDelete : MonoBehaviour {
 
     Transform floor;
-	// Use this for initialization
 	void Start ()
     {
         floor = transform.parent.parent;
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
 		if(floor.position.y <= -1)
